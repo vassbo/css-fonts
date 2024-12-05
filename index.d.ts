4878d0c1c7488df825dadcefd3a564b2431c0263
Kristoffer <kristoffervassbo@gmail.com>
2024-12-05 08:04:53 +0100

    Added TS types

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+declare module "css-fonts" {
+    export interface Family {
+        family: any
+        default: number
+        fonts: Font[]
+    }
+    export interface Font {
+        path: string
+        name: string
+        style: string
+        css: string
+    }
+
+    /**
+     * Gets available fonts in the system
+     *
+     * @returns Fonts orgainzed by family
+     */
+    export function getFonts(): Promise<Family[]>
+}
